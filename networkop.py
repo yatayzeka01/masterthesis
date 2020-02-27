4919bd95d63f5e2addcdab51dc1e6ee8987c2bc4
yatayzeka01 <60409323+yatayzeka01@users.noreply.github.com>
2020-02-27 09:35:38 +0300

    Update networkop.py

--- networkop.py
+++ networkop.py
@@ -7,6 +7,9 @@ from statistics import mean
 from utiles import write_to_file
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+from collections import defaultdict
+
 
 def train(trainData):
     print("Train Process")
@@ -15,7 +18,6 @@ def train(trainData):
     for i in range(2, 7, 1):
         input = []  # numDays x i x 2
         output = []  # numDays
-
 
         # Each day array is consisted of 7 rows
         for day in trainData:
@@ -32,10 +34,9 @@ def train(trainData):
 
             lastPrice = day[i - 1][-1]
             maxPrice = max([item[-1] for item in day[i:]])
-            #heuristic = (maxPrice / lastPrice - 0.97) / (1.03 - 0.97)
+            # heuristic = (maxPrice / lastPrice - 0.97) / (1.03 - 0.97)
             heuristic = maxPrice / lastPrice
             output.append(heuristic)
-
 
         # TODO: change input/output range depending on volume/price, muffle, start at 0.5 and up/down, later in day goes to 0
         lstm = LSTM(12, input_shape=(i, 3))
@@ -51,7 +52,7 @@ def train(trainData):
         print("loss: ", mean(history.history['loss']))
         print("val_loss: ", mean(history.history['val_loss']))
 
-        #plt.plot(history.history['mean_squared_error'])
+        # plt.plot(history.history['mean_squared_error'])
         c = next(color)
         plt.subplot(4, 1, 1)
         plt.plot(history.history['mean_squared_error'], c=c, label=i)
@@ -75,7 +76,7 @@ def train(trainData):
         plt.xlabel('Epoch')
         plt.ylabel('val_loss')
 
-        model_name = "./Models/modelRun_" + str(i) + ".h5"
+        model_name = "./Models/modelRun2_" + str(i) + ".h5"
         if os.path.exists(model_name):
             os.remove(model_name)
         model.save(model_name)
@@ -90,6 +91,8 @@ def test(testData):
     trading_history_file = "trading_history.csv"
     if os.path.exists(trading_history_file):
         os.remove(trading_history_file)
+    plot_pred = defaultdict(list)
+    plot_heuristic = defaultdict(list)
 
     print("\nTest Process")
     modelGain = 1
@@ -106,7 +109,6 @@ def test(testData):
     for day in testData:
 
         dataGain = dataGain * day[-1][-1] / day[0][-1]
-
 
         bought = False
         price = 0
@@ -137,11 +139,14 @@ def test(testData):
             outputeval = []  # numDays
             lastPrice = day[i - 1][-1]
             maxPrice = max([item[-1] for item in day[i:]])
-            #heuristic = (maxPrice / lastPrice - 0.97) / (1.03 - 0.97)
+            # heuristic = (maxPrice / lastPrice - 0.97) / (1.03 - 0.97)
             heuristic = maxPrice / lastPrice
             outputeval.append(heuristic)
 
-            if pred > 0.58 and not bought:
+            plot_pred[i].append(float(pred))
+            plot_heuristic[i].append(heuristic)
+
+            if pred > 0.75 and not bought:
                 # resultseval = model.evaluate([inputeval], outputeval, batch_size=72)
                 write_to_file(trading_history_file, 'Intraday buy: {0}'.format(i))
                 bought = True
@@ -151,13 +156,15 @@ def test(testData):
                 write_to_file(trading_history_file, 'Intraday buy moment: {0}'.format(day[i]))
                 write_to_file(trading_history_file, "=" * 50)
                 buyTime = i
-            elif pred < 0.53 and bought:
+            elif pred < 0.70 and bought:
                 write_to_file(trading_history_file, 'Intraday sell: {0}'.format(i))
                 bought = False
                 modelGain = modelGain * day[i][-1] / price
                 write_to_file(trading_history_file, 'Intraday date: {0}'.format(day[0][0]))
                 write_to_file(trading_history_file, 'Intraday sell moment: {0}'.format(day[i]))
-                write_to_file(trading_history_file, 'Intraday bought at hour {0} for {1}. Sold at hour {2} for {3}.'.format(timechart[buyTime], str(price), timechart[i], str(day[i][-1])))
+                write_to_file(trading_history_file,
+                              'Intraday bought at hour {0} for {1}. Sold at hour {2} for {3}.'.format(
+                                  timechart[buyTime], str(price), timechart[i], str(day[i][-1])))
                 write_to_file(trading_history_file, "=" * 50)
         if bought:
             write_to_file(trading_history_file, 'End of day: {0}'.format(i))
@@ -165,8 +172,27 @@ def test(testData):
             write_to_file(trading_history_file, 'End of day date: {0}'.format(day[0][0]))
             write_to_file(trading_history_file, 'End of day that moment: {0}'.format(day[i]))
             modelGain = modelGain * day[-1][-1] / price
-            write_to_file(trading_history_file, 'End of day bought at hour {0} for {1}. Sold at hour 17:00 for for {2}.'.format(timechart[buyTime], str(price), str(day[i][-1])))
+            write_to_file(trading_history_file,
+                          'End of day bought at hour {0} for {1}. Sold at hour 17:00 for for {2}.'.format(
+                              timechart[buyTime], str(price), str(day[i][-1])))
             write_to_file(trading_history_file, "=" * 50)
 
     print("dataGain: ", dataGain)
     print("modelGain: ", modelGain)
+
+    plot_position = 1
+    for x in range(2, 7):
+        plt.subplots_adjust(bottom=0.08, hspace=1, right=0.85, top=0.95)
+        outputpredToPlotDF = pd.DataFrame({'prediction': plot_pred[x][::10]})
+        outputheuristicToPlotDF = pd.DataFrame({'heuristic': plot_heuristic[x][::10]})
+        plt.subplot(5, 1, plot_position)
+        plt.plot(outputheuristicToPlotDF, color="blue", label="Model {} Heuristic price".format(x))
+        plt.plot(outputpredToPlotDF, color="red", label="Model {} Prediction price".format(x))
+        plt.legend(loc="upper left", fontsize=6)
+        plt.ylabel('Model {0} value'.format(x))
+        plt.title('Test Metrics for Model {}'.format(x), fontdict={'fontsize': 8})
+        plt.xlabel('Hours')
+        plt.ylabel('Value')
+        plot_position += 1
+    plt.savefig("test_metrics_plot.png", dpi=600)
+    plt.close()
