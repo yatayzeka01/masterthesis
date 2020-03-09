149208c7a3948ac2c50a110b08216c19f73554aa
yatayzeka01 <60409323+yatayzeka01@users.noreply.github.com>
2020-03-10 02:37:36 +0300

    Update run.py

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import ast
 from utiles import find_time_interval, prepare
 from networkop import train, test
 import click
+from screeninfo import get_monitors
 
 @click.command()
 @click.argument('command', required=1)
@@ -12,29 +13,34 @@ import click
 @click.option("--start_date", "-sdate", "start_date_inp", required=True, default="01.01.2015", help="Start date to process the input files '01.01.2015' e.g.")
 @click.option("--ti", "-t", "tiflag", required=True, default=1, help="Flag for the additional technical indicators")
 
-def main(command,input_path,output_path,min_time_interval_size,min_number_of_distinct_days,start_date_inp,tiflag):
+def main(command, input_path, output_path, min_time_interval_size, min_number_of_distinct_days, start_date_inp, tiflag):
 	""" Process the input files in the given directory
-		Train Models
-		Make future predictions
+        Train Models
+        Make future predictions
+        COMMAND: prepare|train|predict|overall
+        Sample run command:
+        python3 run.py predict -i data -o info.txt -min_s 7 -min_d 800 -sdate '01.01.2015' -t 1
+    """
 
-		COMMAND: prepare|train|predict|overall
-
-		Sample run command:
-
-		python3 run.py predict -i data -o info.txt -min_s 7 -min_d 800 -sdate '01.01.2015' -t 1
-	"""
 	time_interval, valid_stocks_list = find_time_interval(input_path, output_path, int(min_time_interval_size), int(min_number_of_distinct_days), start_date_inp)
 
 	time_interval = ast.literal_eval(time_interval)
 	time_interval = [n.strip() for n in time_interval]
 
-	data = prepare(input_path, time_interval, valid_stocks_list, tiflag)
+	for m in get_monitors():
+		width = m.width
+		height = m.height
+
+	print("monitor weight:", width)
+	print("monitor height:", height)
+
+	data = prepare(input_path, time_interval, valid_stocks_list, tiflag, width, height)
 	n = int(0.70 * data.shape[0])
 	trainData = data[:n, :, :]
 	testData = data[n:, :, :]
 
-	train(trainData)
-	test(testData)
+	train(trainData, width, height)
+	test(testData, width, height)
 
 if __name__ == "__main__":
 	main()
