d8b12c2bc8b5a9bf0a46a41b78286f025e752858
yatayzeka01 <60409323+yatayzeka01@users.noreply.github.com>
2020-03-11 01:51:24 +0300

    Update networkop.py

--- networkop.py
+++ networkop.py
@@ -7,16 +7,29 @@ from keras.layers import Dense
 from keras.layers import LSTM
 import os
 from statistics import mean
-from utiles import write_to_file
-from utiles import plot_heatmap
+from utiles2 import write_to_file
+from utiles2 import plot_heatmap
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from collections import defaultdict
 
 
-def train(trainData, width, height):
+def train(trainData, width, height, finalDataCoList):
     print("Train Process")
+    print(trainData[:5])
+    finalDataCoDict = {}
+    for i in range(len(finalDataCoList)):
+        finalDataCoDict[finalDataCoList[i]] = i
+
+    del finalDataCoDict["date_time"]
+    del finalDataCoDict["Stock"]
+    finalDataCoList.remove('date_time')
+    finalDataCoList.remove('Stock')
+    print("finalDataCoList")
+    print(finalDataCoList)
+    print("finalDataCoDict")
+    print(finalDataCoDict)
 
     if not os.path.exists('Models'):
         os.mkdir('Models')
@@ -29,25 +42,31 @@ def train(trainData, width, height):
 
         # Each day array is consisted of 7 rows
         for day in trainData:
+            dictlist = []
+
             # e.g. prices and volumes array have the size of 2 when i is 2
             # ex open -1 volume -2 sma -3
-            # with tiFlag : open, sma, stock, volume, date_time
-            prices = array([item[-1] for item in day[:i]])
-            volumes = array([item[-2] for item in day[:i]])
-            smas = array([item[-3] for item in day[:i]])
+            # with tiFlagOld : open, sma, stock, volume, date_time
+            # with tiFlag : date_time, open, volume, sma, stock
+            # prices = array([item[1] for item in day[:i]])
+            for iterkey in finalDataCoDict:
+                globals()[iterkey] = array([item[finalDataCoDict[iterkey]] for item in day[:i]])
+                dictlist.append(iterkey)
+
             # dayInput has the shape of (2,2) at each iteration for the i value 2
-            dayInput = column_stack((prices, volumes, smas)).tolist()
+            dayInput = column_stack((globals()[i] for i in dictlist)).tolist()
+
             # append the dayInput to the input list
             input.append(dayInput)
 
-            lastPrice = day[i - 1][-1]
-            maxPrice = max([item[-1] for item in day[i:]])
+            lastPrice = day[i - 1][finalDataCoDict['Open']]
+            maxPrice = max([item[finalDataCoDict['Open']] for item in day[i:]])
             # heuristic = (maxPrice / lastPrice - 0.97) / (1.03 - 0.97)
             heuristic = maxPrice / lastPrice
             output.append(heuristic)
 
         # TODO: change input/output range depending on volume/price, muffle, start at 0.5 and up/down, later in day goes to 0
-        lstm = LSTM(12, input_shape=(i, 3))
+        lstm = LSTM(12, input_shape=(i, len(finalDataCoList)))
         model = Sequential()
         model.add(lstm)
         model.add(Dense(1))
@@ -70,7 +89,8 @@ def train(trainData, width, height):
             new_input[iterasyon].append(o)
             iterasyon = iterasyon + 1
 
-        inpout = pd.DataFrame(new_input, columns=["Open", "Volume", "Sma", "Heuristic Price"])
+        dictlist.append("Heuristic Price")
+        inpout = pd.DataFrame(new_input, columns=dictlist)
 
         figure_name = "correlation_plot_" + str(i) + ".png"
         plot_heatmap(figure_name, inpout, i, width, height)
@@ -109,7 +129,20 @@ def train(trainData, width, height):
     return
 
 
-def test(testData, width, height):
+def test(testData, width, height, finalDataCoList):
+    finalDataCoDict = {}
+    for i in range(len(finalDataCoList)):
+        finalDataCoDict[finalDataCoList[i]] = i
+
+    del finalDataCoDict["date_time"]
+    del finalDataCoDict["Stock"]
+    finalDataCoList.remove('date_time')
+    finalDataCoList.remove('Stock')
+    print("finalDataCoList")
+    print(finalDataCoList)
+    print("finalDataCoDict")
+    print(finalDataCoDict)
+
     trading_history_file = "trading_history.csv"
     if os.path.exists(trading_history_file):
         os.remove(trading_history_file)
@@ -117,6 +150,7 @@ def test(testData, width, height):
     plot_heuristic = defaultdict(list)
 
     print("\nTest Process")
+    print(testData[:5])
     modelGain = 1
     dataGain = 1
 
@@ -130,7 +164,7 @@ def test(testData, width, height):
 
     for day in testData:
 
-        dataGain = dataGain * day[-1][-1] / day[0][-1]
+        dataGain = dataGain * day[-1][finalDataCoDict['Open']] / day[0][finalDataCoDict['Open']]
 
         bought = False
         price = 0
@@ -148,19 +182,21 @@ def test(testData, width, height):
             elif i == 6:
                 model = model6
 
-            prices = array([item[-1] for item in day[:i]])
-            volumes = array([item[-2] for item in day[:i]])
-            smas = array([item[-3] for item in day[:i]])
+            dictlist = []
 
-            dayInput = column_stack((prices, volumes, smas)).tolist()
+            for iterkey in finalDataCoDict:
+                globals()[iterkey] = array([item[finalDataCoDict[iterkey]] for item in day[:i]])
+                dictlist.append(iterkey)
+
+            dayInput = column_stack((globals()[i] for i in dictlist)).tolist()
             pred = model.predict([[dayInput]])[0][0]
 
             # evaluation
             inputeval = []  # numDays x i x 2
             inputeval.append(dayInput)
             outputeval = []  # numDays
-            lastPrice = day[i - 1][-1]
-            maxPrice = max([item[-1] for item in day[i:]])
+            lastPrice = day[i - 1][finalDataCoDict['Open']]
+            maxPrice = max([item[finalDataCoDict['Open']] for item in day[i:]])
             # heuristic = (maxPrice / lastPrice - 0.97) / (1.03 - 0.97)
             heuristic = maxPrice / lastPrice
             outputeval.append(heuristic)
@@ -171,7 +207,7 @@ def test(testData, width, height):
             if pred > 0.75 and not bought:
                 write_to_file(trading_history_file, 'Intraday buy: {0}'.format(i))
                 bought = True
-                price = day[i][-1]
+                price = day[i][finalDataCoDict['Open']]
                 write_to_file(trading_history_file, 'Intraday buy prediction: {0}'.format(pred))
                 write_to_file(trading_history_file, 'Intraday buy price: {0}'.format(price))
                 write_to_file(trading_history_file, 'Intraday buy moment: {0}'.format(day[i]))
@@ -180,22 +216,22 @@ def test(testData, width, height):
             elif pred < 0.70 and bought:
                 write_to_file(trading_history_file, 'Intraday sell: {0}'.format(i))
                 bought = False
-                modelGain = modelGain * day[i][-1] / price
+                modelGain = modelGain * day[i][finalDataCoDict['Open']] / price
                 write_to_file(trading_history_file, 'Intraday date: {0}'.format(day[0][0]))
                 write_to_file(trading_history_file, 'Intraday sell moment: {0}'.format(day[i]))
                 write_to_file(trading_history_file,
                               'Intraday bought at hour {0} for {1}. Sold at hour {2} for {3}.'.format(
-                                  timechart[buyTime], str(price), timechart[i], str(day[i][-1])))
+                                  timechart[buyTime], str(price), timechart[i], str(day[i][finalDataCoDict['Open']])))
                 write_to_file(trading_history_file, "=" * 50)
         if bought:
             write_to_file(trading_history_file, 'End of day: {0}'.format(i))
             write_to_file(trading_history_file, 'Model Number: {0}'.format(i))
             write_to_file(trading_history_file, 'End of day date: {0}'.format(day[0][0]))
             write_to_file(trading_history_file, 'End of day that moment: {0}'.format(day[i]))
-            modelGain = modelGain * day[-1][-1] / price
+            modelGain = modelGain * day[-1][finalDataCoDict['Open']] / price
             write_to_file(trading_history_file,
                           'End of day bought at hour {0} for {1}. Sold at hour 17:00 for for {2}.'.format(
-                              timechart[buyTime], str(price), str(day[i][-1])))
+                              timechart[buyTime], str(price), str(day[i][finalDataCoDict['Open']])))
             write_to_file(trading_history_file, "=" * 50)
 
     print("dataGain: ", dataGain)
